9843cf808e9db85bc0904d581852928c36494797
Martinez Muzela <martinezmuzela99@gmail.com>
2025-12-08 18:55:50 +0100

    frontend release 2.1

--- lib/api-client.ts
+++ lib/api-client.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "https://student-card-backend-production.up.railway.app/api/v1/"
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "https://student-card-backend-production.up.railway.app/api/v1"
 
 interface RequestOptions extends RequestInit {
   token?: string
